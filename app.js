1a872122f528bf34add24c901b2d917b1af87efe
Douglas Gonçalves <douglasgoncalves@MacBook-Air-de-Douglas.local>
2020-04-21 13:06:53 -0300

    Usuarios Model and Authentication Features

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const path = require("path");
 const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
+require('./models/Postagem');
+const Postagem = mongoose.model('postagens');
+require('./models/Categoria');
+const Categoria = mongoose.model('categorias');
+const usuarios = require('./routes/usuario');
+const passport = require('passport');
+require('./config/auth')(passport);
 
 
 //Configurações
@@ -16,6 +23,9 @@ const flash = require('connect-flash');
     resave: true,
     saveUninitialized: true
   }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(flash());
 
   //Middleware
@@ -35,7 +45,10 @@ const flash = require('connect-flash');
 
   //Mongoose
   mongoose.Promise = global.Promise;
-  mongoose.connect("mongodb://localhost/blogapp").then(()=>{
+  mongoose.connect("mongodb://localhost/blogapp",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(()=>{
     console.log('Conexão com mongoDb funcionando!')
   }).catch((err)=>{
     console.log('Erro ao se conectar com o mongoDb:' + err)
@@ -45,8 +58,64 @@ const flash = require('connect-flash');
   app.use(express.static(path.join(__dirname,'public')));
 
 //Rotas
-app.use('/admin', admin);
+app.get('/', (req,res) => {
+  Postagem.find().lean().populate('categoria').sort({data: 'desc'}).then((postagens)=>{
+    res.render('index', {postagens: postagens});
+  }).catch((err)=>{
+    req.flash('error_msg', 'Houve um erro interno');
+    res.redirect('/404');
+  });
+});
 
+app.get('/404', (req,res) => {
+  res.send('Erro 404!');
+});
+
+app.get('/postagem/:slug', (req,res) => {
+  Postagem.findOne({ slug: req.params.slug }).lean().then(postagem => {
+      if(postagem) {
+          res.render('postagem/index', { postagem: postagem });
+      } else {
+          req.flash('error_msg', 'Esta postagem não existe');
+          res.redirect('/');
+      }
+  }).catch(err => {
+      req.flash('error_msg', 'Houve um erro interno');
+      res.redirect('/');
+  })
+});
+
+app.get('/categorias', (req, res) => {
+  Categoria.find().lean().then(categorias => {
+      res.render('categorias/index', { categorias: categorias });
+  }).catch(err => {
+      req.flash('error_msg', 'Houve um erro interno');
+      res.redirect('/');
+  })
+});
+
+app.get('/categorias/:slug', (req,res) => {
+  Categoria.findOne({ slug: req.params.slug }).lean().then(categoria => {
+      if(categoria) {
+          Postagem.find({ categoria: categoria._id }).then(postagens => {
+              res.render('categorias/postagens', { postagens: postagens, categoria: categoria });
+          }).catch(err => {
+              req.flash('error_msg', 'Houve um erro ao listar os posts');
+              res.redirect('/');
+          })
+      } else {
+          req.flash('error_msg', 'Esta categoria não existe');
+          res.redirect('/');
+      }
+  }).catch(err => {
+      req.flash('error_msg', 'Houve um erro interno ao carregar a página desta categoria');
+      res.redirect('/');
+  })
+});
+
+//Prefixo para grupo de rotas
+app.use('/admin', admin);
+app.use('/usuarios', usuarios);
 
 //Outros
 const PORT = 8089;
